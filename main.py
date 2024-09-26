3010b44a20ad79b9bca1fb93c53c60d4f43961ee
Mr-afk-cell <kboxser2026@kehillahstudent.org>
2024-09-26 10:43:29 -0700

    project

--- main.py
+++ main.py
@@ -1,1 +1,46 @@
 import turtle
+
+#top line
+turtle.forward(180)
+
+#down line
+turtle.right(90)
+turtle.forward(180)
+
+#bottom line
+turtle.right(90)
+turtle.forward(180)
+
+#up line
+turtle.right(90)
+turtle.forward(60)
+
+#across line
+turtle.right(90)
+turtle.forward(60)
+
+#straight line
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(30)
+turtle.right(90)
+turtle.forward(30)
+turtle.right(90)
+turtle.forward(20)
+turtle.right(90)
+turtle.forward(20)
+turtle.right(90)
+turtle.forward(10)
+turtle.right(90)
+turtle.forward(10)
+turtle.right(90)
+turtle.forward(10)
+turtle.right(90)
+turtle.exitonclick()
